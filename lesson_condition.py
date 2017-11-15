bc311814f3d841bfbb83fe6685a7630abe850bc5
Bogdan_Dudin <bohdan010@gmail.com>
2017-11-15 19:02:15 +0200

    init commit

---
+++ lesson_condition.py
@@ -0,0 +1,62 @@
+a = 10
+b = 20
+
+if b != 0:
+    result = a/b
+    print("Result:", result)
+    print("Result: %f.2" % result)
+else:
+    print("Zero division eror")
+
+
+print()
+
+a = 10
+b = 20
+
+is_not_zero = b != 0
+
+def is_not_zero(value):
+    return value != 0
+
+def is__zero(value):
+    return value == 0
+
+if is_not_zero(b):
+    result = a/b
+    print("Result:", result)
+    print("Result: %f.2" % result)
+else:
+    print("Zero division eror")
+
+if is_not_zero(b):
+    print("Zero division eror")
+else:
+    result = a / b
+    print("Result: %f.2" % result)
+
+print()
+
+s = 'Mark Zukerberg'
+if s[0] == 'M' or s[0] == 'm':
+    print("'m' is rhe first lettter")
+
+print()
+
+x = 10
+if x >= 5 and x <= 20:
+    print('INSIDE')
+else:
+    print('OUTSIDE')
+
+print()
+
+def is_millenial(year_of_birth):
+    return year_of_birth > 1981 and year_of_birth <= 2000
+
+if is_millenial (1996):
+    print("I'm millenian!")
+else:
+    print("I'm NOT millenian!")
+
+print()
