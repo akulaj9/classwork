4e7a2ede1e8689ea7f57caffd96147ff5a46d87d
ImpulseProject <101impulse@gmail.com>
2017-12-03 18:59:26 +0200

    inits

--- lesson_nested_loops.py
+++ lesson_nested_loops.py
@@ -1,8 +1,12 @@
-def print_cinema_seats(rows, seats):
+def print_cinema_seats(rows=15, seats=25):
     for i in range (1, rows+1):
         print('Row %d:\t ' % i, end='')
         for j in range (1, seats+1):
-            print('%d' % j, end=' ')
+            is_vip_seat = (5 <= i <= 10) and (10 <= j <=15)
+            if is_vip_seat:
+                print('[%5s]' % j, end='')
+            else:
+                print('%s' % j, end='')
         print()
 
 print_cinema_seats(15, 20)
@@ -10,10 +14,22 @@ print_cinema_seats(3, 4)
 print_cinema_seats()
 
 
-def calt_table(rows=10, columns=10):
+def calc_table(rows=10, columns=10):
     for i in range(1, rows+1):
         for j in range(1, columns+1):
             print(i*j, end='\t')
+        print()
 
 calc_table()
 calc_table(5, 5)
+
+print("*************************")
+
+def chees_table():
+    for i in range(1, 8+1):
+        for symbol in 'ABCDEFGH':
+            print(symbol+str(i), end='\t')
+        print()
+
+
+chees_table()
