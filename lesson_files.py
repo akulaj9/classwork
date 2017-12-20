d0db9ec66bada730a183a1e75959eb8a98ac804e
ImpulseProject <101impulse@gmail.com>
2017-12-20 18:00:07 +0200

    inits

--- lesson_files.py
+++ lesson_files.py
@@ -1,28 +1,31 @@
+# file_path = r'C:\Users\User\Desktop\test.txt'
+# f = open(file_path)
+# print(type(f))
+#
+#
+# # write
+# f = open(file_path, 'w+')
+# for i in range(100):
+#     f.write("Hello world %d\n" % i)
+# f.close()
+#
+#
+# # read
+# f = open(file_path, 'r+')
+# line = f.readline()
+# while line:
+#     print(line)
+#     line = f.readline()
+# f.close()
+#
+#
+
 import pprint
 import string
-
-file_path = r'C:\Users\User\Desktop\test.txt'
-f = open(file_path)
-print(type(f))
-
-#write
-f = open(file_path, 'w+')
-for i in range(100):
-    f.write("Hhello world %d\n" % i)
-f.close()
-
-
-#read
-f = open(file_path, 'r+')
-line = f.readline()
-while line:
-    print(line)
-    line = f.readline()
-f.close()
-
-print("******")
+import csv
 
 def word_counter(file_path, file_path_stop_words, num_limit=20):
+
     f = open(file_path)
     content = f.read().lower()
     lst_words = content.split()
@@ -30,36 +33,32 @@ def word_counter(file_path, file_path_stop_words, num_limit=20):
 
     f2 = open(file_path_stop_words)
     content_stop_words = f2.read()
-    f2.close()
-
-    lst_words = content.split()
     lst_stop_words = content_stop_words.split()
+    f2.close()
 
     print(len(lst_words))
     print(len(lst_stop_words))
 
-    word_stars = {}
+    word_stats = {}
     for word in lst_words:
         if word not in lst_stop_words:
             word = word.strip(string.punctuation)
             if word:
-                if word in word_stars:
-                    word_stars[word] = word_stars[word]+ 1
+                if word in word_stats:
+                    word_stats[word] = word_stats[word] + 1
                 else:
-                    word_stars[word] = 1
+                    word_stats[word] = 1
 
-    #pprint.pprint(word_stars)
+    # pprint.pprint(word_stats)
 
-
-    for key in sorted(word_stars.keys(),
-                      key=lambda key: word_stars[key],
+    for key in sorted(word_stats.keys(),
+                      key=lambda key: word_stats[key],
                       reverse=True)[:num_limit]:
-        print("%s:\t%d" %(key, word_stars[key]))
+        print("%s:\t%d" %(key, word_stats[key]))
 
 
 word_counter('hhgttg.txt', 'stop_words.txt')
 
-print("******")
 
 def get_data_from_csv(filename):
 
@@ -72,5 +71,5 @@ def get_data_from_csv(filename):
 lst_dicts = get_data_from_csv('cities.csv')
 for i, dict in enumerate(lst_dicts):
     print("Entry #", i)
-    for k, v in dict.item():
-        print("\")
+    for k, v in dict.items():
+        print("\t%s: %s" % (k, v))
